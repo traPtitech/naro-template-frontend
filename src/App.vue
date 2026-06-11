5f4e066b10dba26ad6c22fd3f3b3a1ca8cea2b73
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-06-11 21:40:04 +0900

    「Vue入門」のサンプル

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import ClickCounter from './components/ClickCounter.vue'
 import WelcomeMessage from './components/WelcomeMessage.vue'
 </script>
 
 <template>
   <main>
     <WelcomeMessage msg="Webエンジニアになろう講習会へようこそ" />
+    <ClickCounter />
   </main>
 </template>
 
